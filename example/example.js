f9ac8670766c2298a3686daa0aaaaea5cbb1cc9c
sofiisunn <sofiaopenko@gmail.com>
2026-03-19 22:02:53 +0200

    new task

--- example/example.js
+++ example/example.js
@@ -4,13 +4,16 @@ const lib = require('lab2-op');
 
 const gen = lib.generator();
 const tasks = [];
+const priority = ["low", "medium", "high"];
+
 
 for(let i = 0; i < 10; i++) {
     const value = gen.next().value;
-    const task = new lib.Task(value.taskId, "Task " + value.taskId, value.color);
+    let currentPriority = priority[i % priority.length];
+    const task = new lib.Task(value.taskId, "Task " + value.taskId, value.color, currentPriority);
     tasks.push(task);
 }
 
 for(const task of tasks) {
-    console.log(task);
+    console.log(task.taskId, task.title, task.color, task.status, task.createdAt, task.priority);
 }
