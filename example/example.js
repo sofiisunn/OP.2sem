32bb77efac42c30d6b087f3248c5d6d9a2eb7970
sofiisunn <sofiaopenko@gmail.com>
2026-03-24 20:45:09 +0200

    ttl

--- example/example.js
+++ example/example.js
@@ -18,15 +18,19 @@ for(const task of tasks) {
     console.log(task.taskId, task.title, task.color, task.status, task.createdAt, task.priority);
 }
 
-
 function sum(a, b) {
     console.log("Обчислення суми..."); 
     return a + b;
 } 
-const memoSum = lib.memoize(sum, 2);
+const memoSum = lib.memoize(sum, 2, 3000);
+console.log("LRU: ");
 console.log(memoSum(1,2)); 
 console.log(memoSum(2,3)); 
 console.log(memoSum(1,2)); 
 console.log(memoSum(4,5)); 
 console.log(memoSum(2,3));
 console.log(memoSum(1,2)); 
+setTimeout(() => {
+    console.log("TTL: ");
+    console.log(memoSum(1, 2));
+}, 4000);
