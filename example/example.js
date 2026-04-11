134b2fa16725b10f4eda7b402bf746fc4dc1d869
sofiisunn <sofiaopenko@gmail.com>
2026-04-12 00:22:42 +0300

    dequeue

--- example/example.js
+++ example/example.js
@@ -44,13 +44,14 @@ console.log("Priority Map:");
 console.log(pq.priorityMap);
 console.log("Order:");
 console.log(pq.order);
-console.log("Peek highest priority:");
-console.log(pq.peek("highest"));
-console.log("Peek lowest priority:");
-console.log(pq.peek("lowest"));
-console.log("Peek oldest:");
-console.log(pq.peek("oldest"));
-console.log("Peek newest:");
-console.log(pq.peek("newest"));
+console.log("Peek highest priority:", pq.peek("highest"));
+console.log("Peek lowest priority:", pq.peek("lowest"));
+console.log("Peek oldest:", pq.peek("oldest"));
+console.log("Peek newest:", pq.peek("newest"));
+console.log("Dequeue highest priority:", pq.dequeue("highest"));
+console.log("Dequeue lowest priority:", pq.dequeue("lowest"));
+console.log("Dequeue oldest:", pq.dequeue("oldest"));
+console.log("Dequeue newest:", pq.dequeue("newest"));
+
 
 
