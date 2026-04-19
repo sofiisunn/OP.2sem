2185de123408b256db50f27b73e80550074b89dc
sofiisunn <sofiaopenko@gmail.com>
2026-04-19 23:58:58 +0300

    async

--- app/todo.js
+++ app/todo.js
@@ -9,6 +9,7 @@ class TodoApp {
     constructor() {
     this.generator = gen;
     this.queue = new PriorityQueue();
+    this.tasks = new Map();
     }
     addTask() {
     const value = this.generator.next().value;
@@ -16,18 +17,22 @@ class TodoApp {
     const color = value.color;
     const task = new Task(id, "Task " + id, color, 1);
     this.queue.enqueue(task, task.priority);
+    this.tasks.set(task.taskId, task);
     }
     getTask() {
         if(this.queue.order.length === 0) {
             return null;
         }
-        return this.queue.peek('highest');
+        const task = this.queue.peek('highest');
+        return task;
     }
     removeTask() {
         if(this.queue.order.length === 0) {
             return null;
         }
-        return this.queue.dequeue('highest');
+        const task = this.queue.dequeue('highest');
+        this.tasks.delete(task.taskId);
+        return task;
     }
 }
 
