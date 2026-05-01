6c3d75870935e6b4f1e26e6a20aca94af5388ad0
sofiisunn <sofiaopenko@gmail.com>
2026-05-01 23:57:44 +0300

    new

--- app/bundle.js
+++ app/bundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
 const input = document.getElementById('inputTask');
@@ -67,3 +68,40 @@ exampleButton.addEventListener('click', async () => {
 function updateTaskCount() {
 
 }
+
+},{"../lib/async":2,"../lib/task":3}],2:[function(require,module,exports){
+'use strict';
+
+const { Task } = require('./task');
+
+async function* asyncGenerator() {
+    let id = 1;
+    while(id <= 50) {
+        await new Promise(resolve => setTimeout(resolve, 100))
+        yield {
+            taskId: id,
+            title: "Task " + id,
+            color: 'pink',
+            priority: 1
+        };
+        id++;
+    }
+}
+
+module.exports = { asyncGenerator }
+},{"./task":3}],3:[function(require,module,exports){
+'use strict';
+
+class Task {
+    constructor(id, title, color, priority) {
+        this.taskId = id;
+        this.title = title;
+        this.color = color;
+        this.status = "new";
+        this.createdAt = new Date();
+        this.priority = priority;
+    }
+}
+
+module.exports = { Task };
+},{}]},{},[1]);
