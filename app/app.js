93b397c1e674d91b8c4e08d8e7711d73fc55c969
sofiisunn <sofiaopenko@gmail.com>
2026-05-03 16:28:18 +0300

    new

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 import { asyncGenerator } from '../lib/async.js';
 import { Task } from '../lib/task.js';
+import { PriorityQueue } from '../lib/priority_queue.js'
 
 const input = document.getElementById('inputTask');
 const button = document.getElementById('addButton');
@@ -11,21 +12,40 @@ const previewList = document.getElementById('previewList');
 const overlay = document.getElementById('overlay');
 const list = document.getElementById('taskList');
 const addSelected = document.getElementById('addSelected');
+const taskCount = document.getElementById('taskCount');
+const filter = document.getElementById('filter');
 
 let selectedTask = null;
+let id = 1;
+let allTasks = new PriorityQueue();
+
+function updateTaskCount() {
+    taskCount.textContent = 'Задач: ' + allTasks.order.length;
+}
 
 function addTask() {
-    const task = input.value;
-    if (task.trim() === '') return;
+    if (input.value.trim() === '') return;
+    const priority = Number(document.getElementById('priority').value);
+    const task = new Task(id, input.value, null, priority);
+    id++;
+    allTasks.enqueue(task, priority);
     const li = document.createElement('li');
+    li.dataset.id = task.taskId;
+    li.classList.add("priority-" + priority);
+    updateTaskCount();
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.classList.add('task-check');
     li.appendChild(checkbox);
     checkbox.addEventListener('change', () => {
+        const taskId = Number(li.dataset.id);
+        const found = allTasks.order.find(t => t.value.taskId === taskId);
+        if (found) {
+            found.value.done = checkbox.checked;
+        }
         if(checkbox.checked) {
-            li.classList.add('done')
+            li.classList.add('done');
         }
         else {
             li.classList.remove('done')
@@ -33,13 +53,16 @@ function addTask() {
     })
 
     const span = document.createElement('span');
-    span.textContent = task;
+    span.textContent = task.title;
     li.appendChild(span);
     
     const deleteButton = document.createElement('button');
     deleteButton.textContent = '\u00D7';
     deleteButton.addEventListener('click', () => {
+        const taskId = Number(li.dataset.id);
+        allTasks.deleteById(taskId);
         li.remove();
+        updateTaskCount();
     })
     li.appendChild(deleteButton);
     list.appendChild(li);
